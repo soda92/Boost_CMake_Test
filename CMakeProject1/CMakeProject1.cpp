7717ede9a9da25470fe9097f1e8291f14fb2ba80
SodaCris <18463922396@163.com>
2021-10-21 16:03:32 +0800

    添加项目文件。

---
+++ CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,20 @@
+﻿// CMakeProject1.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeProject1.h"
+
+#include <iostream>
+#include <boost/type_index.hpp>
+
+void sample2() {
+	auto&& x = 42;
+
+	std::cout << "x is "
+		<< boost::typeindex::type_id_with_cvr<decltype(x)>()
+		<< std::endl;
+}
+
+int main() {
+	sample2();
+	return 0;
+}
